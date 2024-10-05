7fe4696d3ab4326afdf4076cbd57da97e0b6e69c
Tim Steinke <timjsteinke@gmail.com>
2024-10-05 08:59:57 -0500

    Added restrictions to launch line, added moon base entity

--- scenes/launch_button.gd
+++ scenes/launch_button.gd
@@ -7,7 +7,7 @@ var astronaut
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	pass # Replace with function body.
+	#pass # Replace with function body.
 	#print ("button ready")
 	launch_line = get_node("%LaunchLine")
 	astronaut = get_node("%Astronaut")
@@ -26,17 +26,25 @@ func _process(delta: float) -> void:
 		var current_mouse_local = get_local_mouse_position()
 		current_mouse_local.x -= 30
 		current_mouse_local.y += 20
+		
+		# Limit launching to forward and up
+		if (current_mouse_local.x < -75):
+			current_mouse_local.x = -75
+		if (current_mouse_local.x > -12):
+			current_mouse_local.x = -12
+		if (current_mouse_local.y > 67):
+			current_mouse_local.y = 67
+		if (current_mouse_local.y < 4):
+			current_mouse_local.y = 4
+		
 		launch_line.add_point(current_mouse_local)
 
 		
 	if Input.is_action_just_released("ClickMouse"):
-		print("released")
-		print(get_viewport().get_mouse_position())
 		show_launch_line = false
 		
-		var launch_x = 5* launch_line.get_point_position(1).x + 12
-		var launch_y = 5* launch_line.get_point_position(1).y - 4
-		#print ("launchx: " + str(launch_x) + ", launchy: " + str(launch_y))
+		var launch_x = 3* launch_line.get_point_position(1).x + 12
+		var launch_y = 3* launch_line.get_point_position(1).y - 4
 		
 		astronaut.position.y = astronaut.position.y - 5
 		astronaut.launching = true
@@ -45,18 +53,7 @@ func _process(delta: float) -> void:
 		
 		launch_line.remove_point(1)
 		launch_line.remove_point(0)
-		
-
 
 func _on_pressed() -> void:
 	if Input.is_action_just_pressed("ClickMouse"):
-		print("pressed")
-		print(get_viewport().get_mouse_position())
 		show_launch_line = true
-		#launch_line.add_point(get_viewport().get_mouse_position(), 1)
-	
-	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-	#	print("pressed")
-	#if Input.is_mouse_button_released(MOUSE_BUTTON_LEFT):
-	#	print("released")
-	#	#pass # Replace with function body.
